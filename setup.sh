b31b7abedf599a475e0ab5c57bfa08bf71108878
caot60002 <caot60002@gmail.com>
2026-02-03 12:29:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,4 +12,5 @@ yes | pkg i python-pip
 pip install requests prettytable pycryptodome asyncio pyjwt ecdsa
 export CFLAGS="-Wno-error=implicit-function-declaration"
 pip install psutil
+pip install pytz
 curl -Ls "https://raw.githubusercontent.com/caot60002/test2/refs/heads/main/main.py" -o /sdcard/Download/ToiLaTu.py
