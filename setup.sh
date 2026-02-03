f445d5181dcec2a3acc97524edc206b4d8cb462e
caot60002 <caot60002@gmail.com>
2026-02-03 12:02:03 +0700

    Update script URLs for repository changes and downloads

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd
+if [ -e "/data/data/com.termux/files/home/storage" ]; then
+	rm -rf /data/data/com.termux/files/home/storage
+fi
+termux-setup-storage
+yes | pkg update
+. <(curl https://raw.githubusercontent.com/caot60002/test2/refs/heads/main/termux-change-repo.sh)
+yes | pkg upgrade
+yes | pkg i python
+yes | pkg i python-pip
+pip install requests prettytable pycryptodome asyncio pyjwt ecdsa
+export CFLAGS="-Wno-error=implicit-function-declaration"
+pip install psutil
+curl -Ls "https://raw.githubusercontent.com/caot60002/test2/refs/heads/main/main.py" -o /sdcard/Download/ToiLaTu.py
