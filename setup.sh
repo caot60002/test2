d15662289d5da92ad0b6aa95c2767490f0faaa4d
caot60002 <caot60002@gmail.com>
2026-02-03 12:31:53 +0700

    Add rich library installation to setup script

--- setup.sh
+++ setup.sh
@@ -13,4 +13,5 @@ pip install requests prettytable pycryptodome asyncio pyjwt ecdsa
 export CFLAGS="-Wno-error=implicit-function-declaration"
 pip install psutil
 pip install pytz
+pip install rich
 curl -Ls "https://raw.githubusercontent.com/caot60002/test2/refs/heads/main/main.py" -o /sdcard/Download/ToiLaTu.py
